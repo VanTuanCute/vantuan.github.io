9b658796c854d29d1f0a9dbdede7698bce05bfe5
VanTuanCute <146948996+VanTuanCute@users.noreply.github.com>
2023-10-06 18:36:35 +0700

    Add files via upload

--- sanpham.js
+++ sanpham.js
@@ -1,17 +1,41 @@
 import { NavLink } from "react-router-dom";
 import { Route } from "react-router-dom";
-function SanPham(props){ 
-    return(
-         <div>
-            <img className="san-pham"/>
-                <img src={props.data.hinh_anh}/>
-                <p>{props.data.ten}</p>
-                <p>{props.data.gia}</p>
-                <button> Chọn mua </button>
-        </div> 
-    );
-        
-      
-    
+function SanPham(){
+    return<>
+        <div class="table-responsive">
+            <table class="table table-striped table-sm">
+                <thead>
+            <tr>
+                <th>SẢN PHẨM</th>
+                <th>ĐƠN GIÁ</th>
+                <th>SỐ LƯỢNG</th>
+                <th>THÀNH TIỀN</th>
+                <th></th>
+            </tr>
+            </thead>
+              <tbody>
+                <tr>
+                    <td>Tên sản phẩm</td>
+                    <td>10.000</td>
+                    <td> 2</td>
+                    <td> 20.000</td>
+                    <td>
+                    <a href="#">Xóa</a> | <a href="#">Cập nhật</a>
+                    </td>
+                </tr>
+              </tbody>
+              <div class="right">
+                <ul className="nav nav-pills">
+                <li className="nav-item">
+                    <NavLink to="/Gio-hang" className="btn btn-warning">Tiếp tục mua hàng</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink to="/" className="btn btn-warning">Thanh toán</NavLink>
+                </li>
+                </ul>
+              </div>
+            </table>
+        </div>
+    </>
 }
 export default SanPham;
